676b846e85ff15c5192976968d4d88dca9bd3c02
Siddarth Bisht <ssbdel97@gmail.com>
2018-01-22 00:03:32 +0530

    Add support for chat rooms

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,14 @@ var socket = io();
 console.log(name + ' wants to join room ' + room);
 socket.on('connect', function() {
 	console.log('connected to socket.io!');
+	socket.emit('joinRoom', {
+		name: name,
+		room: room
+	});
 });
+
+jQuery('.room-title').text(room);
+
 socket.on('message', function(message) {
 	var momentTimestamp = moment.utc(message.timestamp);
 	var $message = jQuery('.messages');
@@ -18,6 +25,7 @@ socket.on('message', function(message) {
 
 //Hadles submitting of new message
 var $form = jQuery('#message-form');
+
 $form.on('submit', function(event) {
 	event.preventDefault();
 	var $message = $form.find('input[name=message]');
--- server.js
+++ server.js
@@ -6,14 +6,26 @@ var moment = require('moment');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 app.use(express.static(__dirname + '/public'));
-
+var clientInfo={};
 io.on('connection', function(socket) {
 	console.log('User connected via socket.io');
+	
+	socket.on('joinRoom', function(req) {
+		clientInfo[socket.id] = req;
+		socket.join(req.room);
+		socket.broadcast.to(req.room).emit('message' , {
+			name: 'System',
+			text: req.name + ' has joined!',
+			timestamp: moment().valueOf()
+
+		});
+	});
+
 	socket.on('message', function(message) {
 		console.log('Message recieved: ', message.text);
 		//socket.broadcast.emit('message', message)
 		message.timestamp = moment().valueOf();
-		io.emit('message', message);
+		io.to(clientInfo[socket.id].room).emit('message', message);
 	});
 	var now = moment();
 	socket.emit('message', {
